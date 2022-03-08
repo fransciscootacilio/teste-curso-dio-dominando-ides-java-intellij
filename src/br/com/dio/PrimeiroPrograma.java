a745755ecafd40a1e3f28c6526c12e7c15ae24d5
franciscootacilio <franciscootacilio@gmail.com>
2022-03-08 06:32:09 -0300

    Primeiro commit Intellig

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+       /* int a = 5;
+        int b = 4;
+        System.out.println("Hello Word! " + (a + b));*/
+
+        System.out.println(gato);
+    }
+}
